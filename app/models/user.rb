844dda417644b8431e3cc98a11040cd40d58a894
CY Kalu <cy23@tutanota.com>
2020-12-11 16:19:30 +0100

    Complete User validation and fix syntax error

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,14 +1,16 @@
 class EmailValidator < ActiveModel::EachValidator
   def validate_each(record, attribute, value)
     unless value =~ /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i
-      record.errors[attribute] << (options[:message] || "is not an email")
+      record.errors[attribute] << (options[:message] || 'is not an email')
     end
   end
 end
 
 class User < ApplicationRecord
-	validates  :username, :email, presence: true, uniqueness: true
-	validates  :username, length: { minimum: 3, maximum: 16 }
-	validates  :email, email:true
-	validates	 :password, length { minimum: 8 }
+  has_many :posts
+
+  validates  :username, :email, presence: true, uniqueness: true
+  validates  :username, length: { minimum: 3, maximum: 16 }
+  validates  :email, email: true
+  validates	 :password, length: { minimum: 8 }
 end
